7fcbbe03c057d1850004a8951388dd04240ac173
Brenno-Lucas <brennolucas8@gmail.com>
2022-11-17 10:28:39 -0300

    Requisito 02

---
+++ tests/unit/models/mock/productsModel.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const connection = require('../../../../src/models/connection');
+const { expect } = chai;
+const mock = require('../mock/productsMock');
+const productModel = require('../../../../src/models/products.model');
+const sinon = require('sinon');
+
+describe('Models tests', function () {
+  it('Test findAll function', async function () {
+    sinon.stub(connection, "execute").resolves([mock.products]);
+
+    const result = await productModel.findAll();
+
+    expect(result).to.be.deep.equal(mock.products);
+  });
+
+  afterEach(sinon.restore);
+
+  it('Test findByID function', async function () {
+    sinon.stub(connection, 'execute').resolves([[mock.products]]);
+    const result = await productModel.findById(1);
+    expect(result).to.be.deep.equal(mock.products);
+  });
+});
